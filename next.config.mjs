cbc475931923df2c7f5739b5424ad6aee61c0726
yoshikiaimi <y.aimi@dcworks.jp>
2024-07-31 19:38:08 +0900

    nextConfigの設定を変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // 静的エクスポート有効設定
+  reactStrictMode: true,
+  //   env: {
+  //     ...require(`./config/${process.env.APP_ENV || 'local'}.json`),
+  //   },
+};
 
 export default nextConfig;
